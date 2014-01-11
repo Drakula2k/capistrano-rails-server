e56a407bb2de3a8e4809678d3df6ff8580562194
Drakula2k <drakula2k@gmail.com>
2014-01-11 20:58:58 +0400

    Added setting up config for postfix

--- lib/capistrano-rails-server/recipes/postfix.rb
+++ lib/capistrano-rails-server/recipes/postfix.rb
@@ -6,6 +6,16 @@
     end
     after "deploy:install", "postfix:install"
 
+    desc "Setup postfix configuration"
+    task :setup, roles: :mail do
+      set :hostname, find_servers_for_task(current_task).first.host
+      template "postfix_main.erb", "/tmp/postfix_main_cf"
+      run "#{sudo} mv /etc/postfix/main.cf /tmp/old_postfix_main_cf"
+      run "#{sudo} mv /tmp/postfix_main_cf /etc/postfix/main.cf"
+      run "#{sudo} /etc/init.d/postfix reload"
+    end
+    after "postfix:install", "postfix:setup"
+
     desc "Show postfix stats"
     task :stats, roles: :mail do
       run "#{sudo} ls /var/spool/postfix/incoming|wc -l"
