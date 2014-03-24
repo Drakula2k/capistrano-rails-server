8a78266cd49627fbb6b911e93c0d50cd7e55f456
Drakula2k <drakula2k@gmail.com>
2014-03-24 23:19:07 +0400

    Bugfix

--- lib/capistrano-rails-server/recipes/key.rb
+++ lib/capistrano-rails-server/recipes/key.rb
@@ -2,8 +2,10 @@
   namespace :key do
     desc "Generate deployment key for repository"
     task :generate, roles: :web do
-      run %Q(ssh-keygen -q -N "" -t rsa -C #{user}@#{application} -f ~/.ssh/id_rsa)
-      #run "ssh-add ~/.ssh/id_rsa"
+      unless remote_file_exists?("~/.ssh/id_rsa")
+        run %Q(ssh-keygen -q -N "" -t rsa -C #{user}@#{application} -f ~/.ssh/id_rsa)
+        #run "ssh-add ~/.ssh/id_rsa"
+      end
     end
     after "deploy:install", "key:generate"
 
