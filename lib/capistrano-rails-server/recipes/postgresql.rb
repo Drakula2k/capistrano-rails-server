810a25f4e72014ffa64ff3ac76046bbed0c16a70
Drakula2k <drakula2k@gmail.com>
2014-03-27 10:23:10 +0400

    Added PostgreSQL config template

--- lib/capistrano-rails-server/recipes/postgresql.rb
+++ lib/capistrano-rails-server/recipes/postgresql.rb
@@ -8,7 +8,7 @@
   set_default(:postgresql_locale, "en_US")
 
   namespace :postgresql do
-    desc "Install the latest stable release of PostgreSQL."
+    desc "Install the latest stable release of PostgreSQL"
     task :install, roles: :db, only: {primary: true} do
       run "#{sudo} add-apt-repository -y 'deb http://apt.postgresql.org/pub/repos/apt/ #{system_codename}-pgdg main'"
       run "#{sudo} wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | #{sudo} apt-key add -"
@@ -17,25 +17,35 @@
     end
     after "deploy:install", "postgresql:install"
 
-    desc "Create a database for this application."
+    desc "Setup PostgreSQL configuration"
+    task :setup, roles: :app do
+      run "mkdir -p #{shared_path}/config"
+      template "postgresql.yml.erb", "#{shared_path}/config/database.yml"
+      template "postgresql.conf.erb", "/tmp/postgresql_conf"
+      run "#{sudo} mv /tmp/postgresql_conf /etc/postgresql/9.3/main/postgresql.conf"
+    end
+    after "deploy:setup", "postgresql:setup"
+    after "postgresql:setup", "postgresql:restart"
+
+    desc "Create a database for this application"
     task :create_database, roles: :db, only: {primary: true} do
       run %Q{#{sudo} -u postgres psql -c "create user #{postgresql_user} with password '#{postgresql_password}';"}
       run %Q{#{sudo} -u postgres psql -c "create database #{postgresql_database} owner #{postgresql_user};"}
     end
-    after "deploy:setup", "postgresql:create_database"
-
-    desc "Generate the database.yml configuration file."
-    task :setup, roles: :app do
-      run "mkdir -p #{shared_path}/config"
-      template "postgresql.yml.erb", "#{shared_path}/config/database.yml"
-    end
-    after "deploy:setup", "postgresql:setup"
+    after "postgresql:setup", "postgresql:create_database"
 
     desc "Symlink the database.yml file into latest release"
     task :symlink, roles: :app do
       run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     end
     after "deploy:finalize_update", "postgresql:symlink"
+
+    %w[start stop restart reload force-reload status].each do |command|
+      desc "#{command} PostgreSQL"
+      task command, roles: :db, only: {primary: true} do
+        run "#{sudo} service postgresql #{command}"
+      end
+    end
 
     desc "Rebuild cluster with another encoding and locale. WARNING! This task will remove all databases."
     task :rebuild, roles: :db, only: {primary: true} do
@@ -53,7 +63,7 @@ Do you want to run this task? Otherwise this task will be skipped. (y/n):"
 
     end
     if postgresql_rebuild
-      before "postgresql:create_database", "postgresql:rebuild"
+      before "postgresql:setup", "postgresql:rebuild"
     end
 
     desc "Export and download database to tmp dir"
